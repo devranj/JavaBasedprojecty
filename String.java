202607ac6f5fdccd3955ff419f58cda238b2612f
ranjeet <devopswithranjeet@gmail.com>
2024-02-10 04:01:49 +0000

    String.java file got added in the system

---
+++ String.java
@@ -0,0 +1,13 @@
+Class String{
+public static void main (String[] args){
+
+String name   = "Ranjeet";
+String  Profile = "Solution Architect";
+
+
+System.out.println(name);
+System.out.println(Profile);
+
+}
+}
+
